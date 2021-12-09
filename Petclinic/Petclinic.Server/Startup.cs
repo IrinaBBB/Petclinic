524b50c03e9a74bf990fcbc9a6f002adb487d4f8
Irina <balkarovairina@gmail.com>
2021-12-09 16:27:56 +0100

    add: initial migration

--- Petclinic/Petclinic.Server/Startup.cs
+++ Petclinic/Petclinic.Server/Startup.cs
@@ -1,15 +1,11 @@
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Petclinic.Server.Data;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Petclinic.DataAccess.Data;
 
 namespace Petclinic.Server
 {
@@ -29,6 +25,10 @@ namespace Petclinic.Server
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
